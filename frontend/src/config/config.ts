cf78067a547200004193de2f2ae9dc11a81a6661
Nitin <nitin.agrawal160398@gmail.com>
2024-07-05 12:53:52 +0530

    Updated Dockerfile

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,6 +1,6 @@
 const config = {
     ClientId: process.env.REACT_APP_CLIENT_ID ?? '',
     UserPoolId: process.env.REACT_APP_USER_POOL_ID ?? '',
-    ApiUrl: process.env.REACT_APP_API_URL ?? 'localhost:5000/',
+    ApiUrl: process.env.REACT_APP_API_URL ?? '',
 }
 export default config;
